8a6dc44adef8cd1198af3b9b564bce4c16a8a34e
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2019-08-06 18:04:15 +0400

    impelment view model in merchant project

---
+++ Merchant/Model/PaymentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Merchant.Model
+{
+    public class PaymentViewModel
+    {
+        public int CardNumber { get; set; }
+        //for amount better use decimal accounding to my experience and link: http://net-informations.com/q/faq/float.html
+        //for performance use doucle but if dealing with money use decimal link: https://exceptionnotfound.net/decimal-vs-double-and-other-tips-about-number-types-in-net/
+        public decimal Amount { get; set; }
+        public int ExpiryMonth { get; set; }
+        public int ExpiryYear { get; set; }
+    }
+}
