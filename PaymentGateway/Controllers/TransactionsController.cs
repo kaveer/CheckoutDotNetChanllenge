06c7e7daaaa973a407e09b77a4cc99cc0c27245b
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2019-08-09 00:08:50 +0400

    partial implementation of transaction

--- PaymentGateway/Controllers/TransactionsController.cs
+++ PaymentGateway/Controllers/TransactionsController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using PaymentGateway.Repository.Helper;
+using PaymentGateway.Repository.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Web.Http;
 
 namespace PaymentGateway.Controllers
@@ -10,12 +13,40 @@ namespace PaymentGateway.Controllers
     [RoutePrefix("api/transactions")]
     public class TransactionsController : ApiController
     {
+        private readonly ITransactionRepository _repository;
+        private readonly IAccountsRepository _accountRepository;
+
+        public TransactionsController(ITransactionRepository repository, IAccountsRepository accountRepository)
+        {
+            _repository = repository;
+            _accountRepository = accountRepository;
+        }
+
         [HttpGet]
         [Route("sales")]
         public IHttpActionResult Sales()
         {
-            return Ok("sucecss");
+            try
+            {
+                string token = Request.Headers?.Authorization?.ToString();
+                token = token.Replace("Bearer ", string.Empty);
 
+                if (!_accountRepository.IsTokenValid(token))
+                {
+                    CommonAction.Log(Constants.ApplicationLogType.Transaction_Sales_Token_Invalid.ToString(), "Invalid or expired token");
+                    return BadRequest("Invalid token");
+                }
+
+
+
+
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
         }
     }
 }
