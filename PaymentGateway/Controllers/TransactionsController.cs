6f2c1f63140a6e7e7544a828b607743da20270a0
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2019-08-08 07:41:47 +0400

    Add controller in payment gateway

---
+++ PaymentGateway/Controllers/TransactionsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PaymentGateway.Controllers
+{
+    [RoutePrefix("api/transactions")]
+    public class TransactionsController : ApiController
+    {
+        [HttpGet]
+        [Route("sales")]
+        public IHttpActionResult Sales()
+        {
+            return Ok("sucecss");
+
+        }
+    }
+}
