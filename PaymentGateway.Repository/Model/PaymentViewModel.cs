434a93bcd68a2df17aeb17c0c54a0b89040620f0
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2019-08-09 01:09:17 +0400

    add payment view model in payment gateway

---
+++ PaymentGateway.Repository/Model/PaymentViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Repository.Model
+{
+    class PaymentViewModel
+    {
+        public int CardNumber { get; set; }
+        //for amount better use decimal accounding to my experience and link: http://net-informations.com/q/faq/float.html
+        //for performance use doucle but if dealing with money use decimal link: https://exceptionnotfound.net/decimal-vs-double-and-other-tips-about-number-types-in-net/
+        public decimal Amount { get; set; }
+        public int ExpiryMonth { get; set; }
+        public int ExpiryYear { get; set; }
+    }
+
+    public class OuterMapPaymentViewModel
+    {
+        [Required]
+        public string CardNumber { get; set; }
+        [Required]
+        public string Amount { get; set; }
+        [Required]
+        public string ExpiryMonth { get; set; }
+        [Required]
+        public string ExpiryYear { get; set; }
+    }
+
+    public class PaymentResponseViewModel
+    {
+
+    }
+}
