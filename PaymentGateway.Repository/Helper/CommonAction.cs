ac9bb980146a29442957cf2efee5dbc4e1c099a6
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2019-08-08 14:49:53 +0400

    add constants and common action

---
+++ PaymentGateway.Repository/Helper/CommonAction.cs
@@ -0,0 +1,28 @@
+﻿using PaymentGateway.Datalayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Repository.Helper
+{
+    public static class CommonAction
+    {
+        public static void Log(string type, string details)
+        {
+            using (PaymentGatewayEntities context = new PaymentGatewayEntities())
+            {
+                ApplicationLog data = new ApplicationLog()
+                {
+                    LogType = type,
+                    Details = details,
+                    DateCreated = DateTime.Now
+                };
+
+                context.ApplicationLogs.Add(data);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ PaymentGateway.Repository/Helper/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Repository.Helper
+{
+    public class Constants
+    {
+        public enum ApplicationLogType
+        {
+            Token_Create_Success = 1,
+            Token_Create_Fail = 2,
+            Application_Error = 3
+        }
+    }
+}
