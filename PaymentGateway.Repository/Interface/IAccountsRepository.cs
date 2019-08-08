de63bc91f68549660d371e0f9eb3bc96d12df3ef
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2019-08-08 08:30:18 +0400

    add interface and repository

---
+++ PaymentGateway.Repository/Interface/IAccountsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Repository.Interface
+{
+    public interface IAccountsRepository
+    {
+        string GenerateToken(string merchantId);
+    }
+}
---
+++ PaymentGateway.Repository/Repository/AccountsRepository.cs
@@ -0,0 +1,17 @@
+﻿using PaymentGateway.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Repository.Repository
+{
+    public class AccountsRepository : IAccountsRepository
+    {
+        public string GenerateToken(string merchantId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
