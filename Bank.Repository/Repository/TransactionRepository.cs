cd0f99359722fffe70c1904839bebbac39b2fdd2
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2019-08-09 11:58:03 +0400

    impelment add card in bank

--- Bank.Repository/Repository/TransactionRepository.cs
+++ Bank.Repository/Repository/TransactionRepository.cs
@@ -1,4 +1,5 @@
-﻿using Bank.Repository.Interface;
+﻿using Bank.Datalayer;
+using Bank.Repository.Interface;
 using Bank.Repository.Model;
 using System;
 using System.Collections.Generic;
@@ -12,7 +13,41 @@ namespace Bank.Repository.Repository
     {
         public bool AddCard(GatewayPaymentViewModel item)
         {
-            throw new NotImplementedException();
+            bool result = true;
+
+            if (!IsCardValid(item))
+                return false;
+
+            using (BankEntities context = new BankEntities())
+            {
+                context.CardDetails.Add(new CardDetail()
+                {
+                    CardNumber = item.CardNumber.ToString(),
+                    ExpiryMonth = item.ExpiryMonth,
+                    ExpiryYear = item.ExpiryYear,
+                    TotalAmount = item.Amount
+                });
+
+                context.SaveChanges();
+            }
+
+            return result;
+        }
+
+        private bool IsCardValid(GatewayPaymentViewModel item)
+        {
+            bool result = true;
+
+            if (item?.Amount == 0 || item?.CardNumber == 0 || item?.ExpiryMonth == 0 || item?.ExpiryYear ==0)
+                return false;
+
+            if (item?.ExpiryMonth < DateTime.Now.Month)
+                return false;
+
+            if (item?.ExpiryYear < DateTime.Now.Year)
+                return false;
+
+            return result;
         }
 
         public PaymentResponseViewModel SalesTransaction(PaymentResponseViewModel item)
