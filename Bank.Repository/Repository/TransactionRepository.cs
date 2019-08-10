2c62d1671c9854e2ea001c09747a2502d2ac5758
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2019-08-10 12:45:41 +0400

    full implementation of  tansaction in bank api

--- Bank.Repository/Repository/TransactionRepository.cs
+++ Bank.Repository/Repository/TransactionRepository.cs
@@ -38,7 +38,7 @@ namespace Bank.Repository.Repository
         {
             bool result = true;
 
-            if (item?.Amount == 0 || item?.CardNumber == 0 || item?.ExpiryMonth == 0 || item?.ExpiryYear ==0)
+            if (item?.Amount == 0 || item?.CardNumber == 0 || item?.ExpiryMonth == 0 || item?.ExpiryYear == 0)
                 return false;
 
             if (item?.ExpiryMonth < DateTime.Now.Month)
@@ -52,7 +52,120 @@ namespace Bank.Repository.Repository
 
         public PaymentResponseViewModel SalesTransaction(PaymentResponseViewModel item)
         {
+            PaymentResponseViewModel result = new PaymentResponseViewModel();
+
+            if (IsModelValid(item))
+            {
+                using (BankEntities context = new BankEntities())
+                {
+
+                    var debitAccount = context.CardDetails
+                                    .Where(x=> long.Parse(x.CardNumber) == item.Payment.CardNumber)
+                                    .FirstOrDefault();
+
+                    if (debitAccount.TotalAmount < item.Payment.Amount)
+                        return result = InValidTransaction("Insufficent amount. Transaction aborted");
+
+                    debitAccount.TotalAmount = (debitAccount.TotalAmount - item.Payment.Amount);
+                    context.SaveChanges();
+
+                    var merchant = context.CardDetails
+                                        .Where(x => long.Parse(x.CardNumber) == item.Merchant.CardNumber)
+                                        .FirstOrDefault();
+
+                    merchant.TotalAmount = merchant.TotalAmount + item.Payment.Amount;
+
+                    return result = new PaymentResponseViewModel()
+                    {
+                        TransactionId = Guid.NewGuid().ToString(),
+                        Details = new GatewayTransactionDetailsViewModel()
+                        {
+                            IsSuccess = true,
+                            Code = 2000,
+                            Details = string.Format("Transaction successfull between debit: {0} and and credit {1} account. {2} was successfully credited", item.Payment.CardNumber, item.Merchant.CardNumber, item.Payment.Amount),
+                            Message = "Transaction successfull",
+                            TransactionDate = DateTime.Now
+                        },
+                        Payment = item.Payment,
+                        Merchant = item.Merchant
+                    };
+                }
+            }
+            else
+            {
+                return result = InValidTransaction("Invalid model for transaction");
+            }
+
+        }
+
+        private PaymentResponseViewModel InValidTransaction(string message)
+        {
+            PaymentResponseViewModel result = new PaymentResponseViewModel()
+            {
+                TransactionId = "0",
+                Merchant = new GatewayMerchantViewModel(),
+                Payment = new GatewayPaymentViewModel(),
+                Details = new GatewayTransactionDetailsViewModel()
+                {
+                    IsSuccess = false,
+                    Message = message,
+                    Details = message,
+                    Code = 2039,
+                    TransactionDate = DateTime.Now
+                }
+            };
+
             throw new NotImplementedException();
+        }
+
+        private bool IsModelValid(PaymentResponseViewModel item)
+        {
+            bool result = true;
+
+            if (!IsCardValid(item?.Payment))
+                return false;
+
+            if (!IsMerchantValid(item?.Merchant))
+                return false;
+
+            if (!IsCardExist(item.Payment.CardNumber))
+                return false;
+
+            return result;
+        }
+
+        private bool IsMerchantValid(GatewayMerchantViewModel merchant)
+        {
+            bool result = true;
+
+            if (merchant.CardNumber == 0 || merchant.CardNumber == 0)
+                return false;
+
+            if (merchant.ExpiryMonth < DateTime.Now.Month)
+                return false;
+
+            if (merchant.ExpiryYear < DateTime.Now.Year)
+                return false;
+
+            if (!IsCardExist(merchant.CardNumber))
+                return false;
+
+            return result;
+        }
+
+        private bool IsCardExist(long cardNumber)
+        {
+            bool result = true;
+
+            using (BankEntities context = new BankEntities())
+            {
+                context.CardDetails
+                           .Where(x => long.Parse(x.CardNumber) == cardNumber)
+                           .FirstOrDefault();
+
+            }
+
+            return result;
         }
     }
 }
