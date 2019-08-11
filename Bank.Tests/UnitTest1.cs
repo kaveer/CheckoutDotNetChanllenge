6e65a434a2f991941809b3fbbc5ea598df33914f
kaveer rajcoomar <kaveer.rajcoomar@hotmail.com>
2019-08-12 00:55:44 +0400

    add moq reference

--- Bank.Tests/UnitTest1.cs
+++ Bank.Tests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using Bank.Controllers;
+using Bank.Repository.Interface;
+using Bank.Repository.Model;
+using Bank.Repository.Repository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Bank.Tests
 {
@@ -7,8 +13,58 @@ namespace Bank.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SaveCard()
         {
+            //Arrange
+            TransactionRepository test = new TransactionRepository();
+
+            List<GatewayPaymentViewModel> collection = new List<GatewayPaymentViewModel>();
+
+            GatewayPaymentViewModel validCard = new GatewayPaymentViewModel()
+            {
+                Amount = 10000,
+                CardNumber = 378282246310005,
+                ExpiryMonth = 12,
+                ExpiryYear = 2030
+            };
+            collection.Add(validCard);
+
+            GatewayPaymentViewModel merchantCard = new GatewayPaymentViewModel()
+            {
+                Amount = 10000M,
+                CardNumber = 5312794635897512,
+                ExpiryMonth = 12,
+                ExpiryYear = 2030
+            };
+            collection.Add(merchantCard);
+
+            GatewayPaymentViewModel invalidMonth = new GatewayPaymentViewModel()
+            {
+                Amount = 10000M,
+                CardNumber = 5312794635897512,
+                ExpiryMonth = 01,
+                ExpiryYear = 2030
+            };
+            collection.Add(invalidMonth);
+
+            GatewayPaymentViewModel invalidYear = new GatewayPaymentViewModel()
+            {
+                Amount = 10000M,
+                CardNumber = 5312794635897512,
+                ExpiryMonth = 12,
+                ExpiryYear = 2015
+            };
+            collection.Add(invalidYear);
+
+
+
+            //Act
+            foreach (var item in collection)
+            {
+                var result = test.AddCard(item);
+            }
+
+            //Assert
         }
     }
 }
